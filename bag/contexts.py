aa3e29cd95bd74c47e04c64e993ea59560c04fbe
Morad17 <morad123@hotmail.co.uk>
2020-11-14 14:43:49 +0000

    created inc dec buttons

--- bag/contexts.py
+++ bag/contexts.py
@@ -11,6 +11,7 @@ def bag_contents(request):
     bag = request.session.get('bag', {})
 
     for item_id, item_data in bag.items():
+        print(item_data)
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
