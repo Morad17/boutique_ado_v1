fc29a1d1195ddb289d151b67b2bee855eeb8a72e
Morad17 <morad123@hotmail.co.uk>
2020-11-18 12:46:29 +0000

    created form.py for orders

--- checkout/models.py
+++ checkout/models.py
@@ -35,7 +35,7 @@ class Order(models.Model):
         accounting for delivery costs.
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
-        if self.order_total < settings.FREE_DELIVER_THRESHOLD:
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
@@ -54,9 +54,9 @@ class Order(models.Model):
     def __str__(self):
         return self.order_number
 
-class OrderLinkeItem(models.Model):
+class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True) # XS,S, M, L,XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
@@ -69,5 +69,5 @@ class OrderLinkeItem(models.Model):
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
-    def __str__(self):
+    def __str__(self): 
         return f'SKU {self.product.sku} on order {self.order.order_number}'
