72cc739fc1d4a3df97823104a2a44384edbaf0c8
Morad17 <morad123@hotmail.co.uk>
2020-11-22 14:06:10 +0000

    fixed signup errors

--- profiles/models.py
+++ profiles/models.py
@@ -5,12 +5,15 @@ from django.dispatch import receiver
 
 from django_countries.fields import CountryField
 
+
 class UserProfile(models.Model):
     """
     A user profile model for maintaining default 
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
@@ -21,12 +24,12 @@ class UserProfile(models.Model):
         return self.user.username
 
 
-    @receiver(post_save, sender=User)
-    def create_or_update_user_profile(sender, instance, created, **kwargs):
-        """
-        Create or Update the user profile
-        """
-        if created:
-            UserProfile.objects.create(user=instance)
-        # Existing users: just save the profile
-        instance.userprofile.save()
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or Update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
